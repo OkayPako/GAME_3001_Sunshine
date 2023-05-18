0761c8b228ab636b0bd6d1f0616a5ff738502a53
Raphael Flores <raphflores.rf@gmail.com>
2023-05-18 13:01:07 -0400

    acceleration, velocity, and position

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,18 +1,70 @@
 #include "rlImGui.h"
+#include "Math.h"
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
+
+Vector2 WraparoundScreen(Vector2 position)
+{
+    Vector2 outPosition =
+    {
+        (int)(position.x + SCREEN_WIDTH) % SCREEN_WIDTH,
+        (int)(position.y + SCREEN_HEIGHT) % SCREEN_HEIGHT
+    };
+
+    return outPosition;
+}
 
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    SetTargetFPS(120);
+    SetTargetFPS(200);
 
+    rlImGuiSetup(true); // Sets up imgui
+
+    Vector2 position = { 100, 100 };
+    Vector2 urMom = { 100, 400 };
+    Vector2 velocity = { 10, 0 };
+    Vector2 acceleration = { 0, 50 };
+    float maxSpeed = 1000;
+
+    bool useGUI = false;
     while (!WindowShouldClose())
     {
+        const float deltaTime = GetFrameTime();
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        if (IsKeyDown(KEY_GRAVE)) useGUI = !useGUI;
+        if (useGUI)
+        {
+            rlImGuiBegin();
+            
+            ImGui::SliderFloat2("Position", &(position.x), 0, SCREEN_WIDTH);
+            ImGui::SliderFloat2("Velocity", &(velocity.x), -maxSpeed, maxSpeed);
+            ImGui::SliderFloat2("Acceleration", &(acceleration.x), -maxSpeed, maxSpeed);
+
+            rlImGuiEnd();
+        }
+
+        Vector2 displacement = velocity * deltaTime; // px/s * s = px
+        position = position + displacement;
+
+        Vector2 deltaV = acceleration * deltaTime;
+        velocity = velocity + deltaV; // px/s/s/s * s = px/s
+
+        position = position + displacement * acceleration * deltaTime * deltaTime * 0.5f;
+
+        position = WraparoundScreen(position);
+
         DrawText("Hello World!", 16, 9, 20, RED);
+
+        DrawCircleV(position, 50, GREEN);
+        DrawCircleGradient(urMom.x, urMom.y, 50, LIGHTGRAY, BLACK);
+
+        
         DrawFPS(10, 100);
+
         EndDrawing();
     }
 
