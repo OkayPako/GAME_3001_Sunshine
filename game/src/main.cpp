f500bc22e1981bcc48b1202bfcbde4da0f177f52
Raphael Flores <raphflores.rf@gmail.com>
2023-05-18 10:59:11 -0400

    Started Lab 2

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -5,13 +5,14 @@
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
-    SetTargetFPS(60);
+    SetTargetFPS(120);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World!", 16, 9, 20, RED);
+        DrawFPS(10, 100);
         EndDrawing();
     }
 
