ca65c4a4c08584ca772430831dcbd8c7966a35cc
Raphael Flores <raphflores.rf@gmail.com>
2023-05-25 11:11:20 -0400

    Removed some other random code

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -138,22 +138,6 @@ public:
 
         return steering;
     }
-
-    float Clamp(float value, float min, float max)
-    {
-        if (value < min)
-        {
-            return min;
-        }
-        else if (value > max)
-        {
-            return max;
-        }
-        else
-        {
-            return value;
-        }
-    }
 };
 
 int main()
