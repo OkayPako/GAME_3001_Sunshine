724ac6ec2f7e323cd1baa2a8d3758e7d76d02ba1
Raphael Flores <raphflores.rf@gmail.com>
2023-06-06 18:59:14 -0400

    Added Seek and Flee

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -8,6 +8,25 @@
 #define SCREEN_WIDTH 1920
 #define SCREEN_HEIGHT 1080
 
+// Calculates a steering force that directs the agent towards a target position while limiting its maximum speed.
+Vector2 Seek(const Vector2& pos, const Rigidbody& rigidbody, float maxSpeed)
+{
+    return Normalize(pos - rigidbody.pos) * maxSpeed - rigidbody.vel;
+}
+
+// Calculates a steering force that directs a target position towards the agent while limiting its maximum speed.
+Vector2 Flee(const Vector2& pos, const Rigidbody& rigidbody, float maxSpeed)
+{
+    return Normalize(rigidbody.pos - pos) * maxSpeed - rigidbody.vel;
+}
+
+// Checks if a line segment intersects with a obstacle, which is used later to detect collisions between the seeker and an obstacle.
+bool CheckCollisionLineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
+{
+    Vector2 nearest = NearestPoint(lineStart, lineEnd, circlePosition);
+    return DistanceSqr(nearest, circlePosition) <= circleRadius * circleRadius;
+}
+
 int main(void)
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Fishies");
