55fbb518ab66a26b0761b534161d0430a41ea291
Raphael Flores <raphflores.rf@gmail.com>
2023-06-13 19:10:28 -0400

    Red square for movement! :)

--- game/src/Tilemap.h
+++ game/src/Tilemap.h
@@ -2,8 +2,8 @@
 #include "raylib.h"
 #include "TileCoord.h"
 
-#define MAP_WIDTH 40
-#define MAP_HEIGHT 23
+#define MAP_WIDTH 20
+#define MAP_HEIGHT 12
 
 enum class Tile
 {
@@ -11,9 +11,9 @@ enum class Tile
     grass,
     water,
     wall,
+    player, // New player tile type :)
     count
 };
-
 
 class Tilemap
 {
@@ -21,8 +21,10 @@ private:
     bool isTranversable[(int)Tile::count];
 
 public:
-    float tileSizeX = 32; // width of a tile in pixels
-    float tileSizeY = 32; // height of a tile in pixels
+    float tileSizeX = 64; // width of a tile in pixels
+    float tileSizeY = 64; // height of a tile in pixels
+
+    TileCoord playerPosition;  // New player position variable
 
     Color tileColors[(int)Tile::count];
 
@@ -46,15 +48,28 @@ public:
         return {coordinate.x * tileSizeX, coordinate.y * tileSizeY};
     }
 
-    Vector2 GetTileAtScreenPos(Vector2 coordinate)
-    {
+    // The function calculates the tile coordinate based on the screen coordinate by dividing the x and y values by the tile size and converting them to integers.
+    // It returns the calculated tile coordinate as a Vector2 object.
 
+    Vector2 GetTileAtScreenPosition(Vector2 coordinate)
+    {
+        return { coordinate.x / tileSizeX, coordinate.y / tileSizeY };
     }
+
+    // The function first checks if the given coordinate is within the bounds of the tilemap using conditional statements.
+    // If the coordinate is valid, it retrieves the tile type at that coordinate from the tiles array.
+    // It then uses the isTranversable array to determine if the tile is traversable by converting the tile type to an integer and accessing the corresponding element in the array.
+    // If the coordinate is out of bounds, it returns false to indicate that the tile is not traversable.
 
     bool isTileTraversable(TileCoord coordinate)
     {
-        Tile tileType = tiles[coordinate.x][coordinate.y];
-        return isTranversable[(int)Tile::count];
+        if (coordinate.x >= 0 && coordinate.x < MAP_WIDTH && coordinate.y >= 0 && coordinate.y < MAP_HEIGHT)
+        {
+            Tile tileType = tiles[coordinate.x][coordinate.y];
+            return isTranversable[static_cast<int>(tileType)];
+        }
+
+        return false; // Return false for out-of-bounds coordinates
     }
 
     void Randomize(int chanceOfFloor = 50)
@@ -80,6 +95,18 @@ public:
                     tiles[x][y] = Tile::wall;
                 }
             }
+
+            // Randomly set player position on a floor tile
+            int playerX, playerY;
+
+            do
+            {
+                playerX = rand() % MAP_WIDTH;
+                playerY = rand() % MAP_HEIGHT;
+            } 
+            while (tiles[playerX][playerY] != Tile::floor);
+
+            playerPosition = TileCoord(playerX, playerY);
         }
     }
 
@@ -89,13 +116,14 @@ public:
         {
             for (int y = 0; y < MAP_HEIGHT; y++)
             {
-                // Vector2 coordinate = GetScreenPositionOfTile({x,y});
-                // DrawRectangle(coordinate, {tileSizeX, tileSizeY});
-
                 Tile tileType = tiles[x][y];
                 Color tileColor = tileColors[(int)tileType];
 
                 DrawRectangle(x * tileSizeX, y * tileSizeY, tileSizeX, tileSizeY, tileColor);
+
+                // Draw player sprite
+                Vector2 playerScreenPosition = GetScreenPositionOfTile(playerPosition);
+                DrawRectangle(static_cast<int>(playerScreenPosition.x), static_cast<int>(playerScreenPosition.y), tileSizeX, tileSizeY, RED);
             }
         }
     }
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,14 +1,12 @@
 #include "rlImGui.h"
+#include "Tilemap.h"
+#include "raylib.h"
 #include <time.h>
-#include"Tilemap.h"
-
 
 #define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
-
+#define SCREEN_HEIGHT 768
 
 Tilemap map;
-
 
 int main(void)
 {
@@ -22,10 +20,19 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-
         map.Draw();
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        // Player movement
+        if (IsKeyPressed(KEY_W) && map.isTileTraversable(map.playerPosition - TileCoord(0, 1)))
+            map.playerPosition -= TileCoord(0, 1);
+        if (IsKeyPressed(KEY_S) && map.isTileTraversable(map.playerPosition + TileCoord(0, 1)))
+            map.playerPosition += TileCoord(0, 1);
+        if (IsKeyPressed(KEY_A) && map.isTileTraversable(map.playerPosition - TileCoord(1, 0)))
+            map.playerPosition -= TileCoord(1, 0);
+        if (IsKeyPressed(KEY_D) && map.isTileTraversable(map.playerPosition + TileCoord(1, 0)))
+            map.playerPosition += TileCoord(1, 0);
 
         if (IsKeyPressed(KEY_GRAVE)) useGUI = !useGUI;
         if (useGUI)
@@ -37,6 +44,101 @@ int main(void)
             }
             rlImGuiEnd();
         }
+
+        // Draw connections between traversable tiles
+
+        // checks the traversability of the adjacent tiles individually in each cardinal direction (north, south, east, and west). 
+        // If an adjacent tile in any of these directions is traversable, it calculates the center position of the adjacent tile 
+        // and draws a line from the center of the current tile to the center of the adjacent tile
+
+        if (useGUI)
+        {
+            for (int x = 0; x < MAP_WIDTH; x++)
+            {
+                for (int y = 0; y < MAP_HEIGHT; y++)
+                {
+                    if (map.isTileTraversable(TileCoord(x, y)))
+                    {
+                        Vector2 tileCenter = map.GetScreenPositionOfTile(TileCoord(x, y)) + Vector2{ map.tileSizeX / 2, map.tileSizeY / 2 };
+
+                        int adjacentX, adjacentY;
+
+                        // Check North
+                        adjacentX = x;
+                        adjacentY = y - 1;
+                        if (adjacentY >= 0 && map.isTileTraversable(TileCoord(adjacentX, adjacentY)))
+                        {
+                            Vector2 adjacentTileCenter = map.GetScreenPositionOfTile(TileCoord(adjacentX, adjacentY)) + Vector2{ map.tileSizeX / 2, map.tileSizeY / 2 };
+                            DrawLineEx(tileCenter, adjacentTileCenter, 2, DARKGREEN);
+                        }
+
+                        // Check South
+                        adjacentX = x;
+                        adjacentY = y + 1;
+                        if (adjacentY < MAP_HEIGHT && map.isTileTraversable(TileCoord(adjacentX, adjacentY)))
+                        {
+                            Vector2 adjacentTileCenter = map.GetScreenPositionOfTile(TileCoord(adjacentX, adjacentY)) + Vector2{ map.tileSizeX / 2, map.tileSizeY / 2 };
+                            DrawLineEx(tileCenter, adjacentTileCenter, 2, DARKGREEN);
+                        }
+
+                        // Check East
+                        adjacentX = x + 1;
+                        adjacentY = y;
+                        if (adjacentX < MAP_WIDTH && map.isTileTraversable(TileCoord(adjacentX, adjacentY)))
+                        {
+                            Vector2 adjacentTileCenter = map.GetScreenPositionOfTile(TileCoord(adjacentX, adjacentY)) + Vector2{ map.tileSizeX / 2, map.tileSizeY / 2 };
+                            DrawLineEx(tileCenter, adjacentTileCenter, 2, DARKGREEN);
+                        }
+
+                        // Check West
+                        adjacentX = x - 1;
+                        adjacentY = y;
+                        if (adjacentX >= 0 && map.isTileTraversable(TileCoord(adjacentX, adjacentY)))
+                        {
+                            Vector2 adjacentTileCenter = map.GetScreenPositionOfTile(TileCoord(adjacentX, adjacentY)) + Vector2{ map.tileSizeX / 2, map.tileSizeY / 2 };
+                            DrawLineEx(tileCenter, adjacentTileCenter, 2, DARKGREEN);
+                        }
+
+                        DrawCircle(static_cast<int>(tileCenter.x), static_cast<int>(tileCenter.y), 8, RED);
+                    }
+                }
+            }
+        }
+
+        // Draw connections between traversable tiles that are on all axis
+
+        //if (useGUI)
+        //{
+        //    for (int x = 0; x < MAP_WIDTH; x++)
+        //    {
+        //        for (int y = 0; y < MAP_HEIGHT; y++)
+        //        {
+        //            if (map.isTileTraversable(TileCoord(x, y)))
+        //            {
+        //                Vector2 tileCenter = map.GetScreenPositionOfTile(TileCoord(x, y)) + Vector2{ map.tileSizeX / 2, map.tileSizeY / 2 };
+        //
+        //                for (int dx = -1; dx <= 1; dx++)
+        //                {
+        //                    for (int dy = -1; dy <= 1; dy++)
+        //                    {
+        //                        if (dx == 0 && dy == 0)
+        //                            continue;
+        //
+        //                        int adjacentX = x + dx;
+        //                        int adjacentY = y + dy;
+        //
+        //                        if (adjacentX >= 0 && adjacentX < MAP_WIDTH && adjacentY >= 0 && adjacentY < MAP_HEIGHT && map.isTileTraversable(TileCoord(adjacentX, adjacentY)))
+        //                        {
+        //                            Vector2 adjacentTileCenter = map.GetScreenPositionOfTile(TileCoord(adjacentX, adjacentY)) + Vector2{ map.tileSizeX / 2, map.tileSizeY / 2 };
+        //                            DrawLineEx(tileCenter, adjacentTileCenter, 2, DARKGREEN);
+        //                        }
+        //                    }
+        //                }
+        //                DrawCircle(static_cast<int>(tileCenter.x), static_cast<int>(tileCenter.y), 10, RED);
+        //            }
+        //        }
+        //    }
+        //}
 
         DrawText("Tile Map!", 20, 10, 20, RAYWHITE);
         EndDrawing();
